f97889e49b2d678ad5bc3a5864cc98789391cd9c
Aniruddha <120139105+Ani111112@users.noreply.github.com>
2023-05-16 00:01:20 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+   RWOnly ro = new RWOnly();
+   //setting the name
+   ro.setName("Aniruddha");
+   //setting the age
+   ro.setAge(24);
+   //getting the name and print it
+   System.out.println(ro.getName());
+   System.out.println(ro.getAge());
 }
