fdc3cd2614d56899267edc2a0817fe173a2289b6
Aniruddha <120139105+Ani111112@users.noreply.github.com>
2023-05-15 23:57:34 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class RWOnly {
-    
+    //implimenting encapsulation
+    private String name;
+    private int age;
+    public setName(String name){
+        this.name = name;
+    }
+    public setAge(int age){
+        this.age = age;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
 }
